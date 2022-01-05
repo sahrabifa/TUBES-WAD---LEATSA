36db9ff7579a2c990ebfb4f63a4b303923ae7ad6
Fatih <Fadhillahfatih@gmail.com>
2022-01-05 21:44:30 +0700

    update

--- TUBESWADFIX/routes/web.php
+++ TUBESWADFIX/routes/web.php
@@ -13,39 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/dashboard', function () {
-    return view('index');
-})->name('dashboard');
+// Route::get('/', function () {
+//     return view('login');
+// });
 
 Route::get('/registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
 
 Route::get('/login', [\App\Http\Controllers\AuthController::class, 'index'])->name('login');
 Route::post('/login-process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login.process');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/me', [\App\Http\Controllers\AuthController::class, 'me'])->name('me');
-    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
-    Route::post('/profile', [\App\Http\Controllers\AuthController::class, 'updateProfile'])->name('profile.update');
-
-    Route::get('/donor', [\App\Http\Controllers\PMIController::class, 'donor'])->name('donor');
-    Route::get('/donor/create', [\App\Http\Controllers\PMIController::class, 'donorCreate'])->name('donor.create');
-    Route::post('/donor', [\App\Http\Controllers\PMIController::class, 'donorStore'])->name('donor.store');
-    Route::get('/donor/{donor}/download', [\App\Http\Controllers\PMIController::class, 'downloadDonor'])->name('donor.download');
-
-    Route::get('/schedule/{schedule}/donor', [\App\Http\Controllers\PMIController::class, 'showScheduleDonor'])->name('schedule.show.donor');
-
-    Route::get('/user/{user}/edit', [\App\Http\Controllers\PMIController::class, 'editUser'])->name('user.edit');
-    Route::put('/user/{user}', [\App\Http\Controllers\PMIController::class, 'updateUser'])->name('user.update');
-    Route::delete('/user/{user}', [\App\Http\Controllers\PMIController::class, 'deleteUser'])->name('user.delete');
-});
-
-Route::get('/donor/about', [\App\Http\Controllers\PMIController::class, 'aboutDonor'])->name('donor.about');
-Route::get('/blood-stock', [\App\Http\Controllers\PMIController::class, 'bloodStock'])->name('blood-stock');
-Route::get('/contact', [\App\Http\Controllers\PMIController::class, 'contact'])->name('contact');
-Route::get('/schedule', [\App\Http\Controllers\PMIController::class, 'schedule'])->name('schedule');
-Route::get('/find-friend', [\App\Http\Controllers\PMIController::class, 'findFriend'])->name('find-friend');
