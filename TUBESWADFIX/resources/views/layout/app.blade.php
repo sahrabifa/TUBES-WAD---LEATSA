324afef31e6ea53b57e60b0cb8669466545f72b3
sahrabifa <sabifa0251@gmail.com>
2022-01-06 16:27:16 +0700

    main

---
+++ TUBESWADFIX/resources/views/layout/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Ayo Donor</title>
+    @include('layout.head')
+</head>
+<body>
+
+@include('layout.navbar')
+
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block mt-3 mx-5">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block mt-3 mx-5">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+<div id="main" class="row">
+
+    @yield('content')
+
+</div>
+
+<footer>
+    @include('layout.footer')
+</footer>
+
+</body>
+</html>
+
+
