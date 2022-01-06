ce0cc35ec6ee6ba002a1d0bab859ca3be64577f7
sahrabifa <sabifa0251@gmail.com>
2022-01-06 16:34:19 +0700

    main

---
+++ TUBESWADFIX/resources/views/pmi/aboutDonor.blade.php
@@ -0,0 +1,111 @@
+@extends('layout.app')
+@section('content')
+<div class="container py-5">
+
+    <p>Salah satu kegiatan PMI yang paling dikenal masyarakat adalah donor darah. Menyumbangkan sebagian darah untuk
+        kemudian disalurkan kepada yang membutuhkan menjadi suatu sumbangan berarti dalam kehidupan sosial
+        bermasyarakat. Tidak membutuhkan persyaratan sulit untuk menjadi calon donor.</p>
+    <b>Syarat Untuk Menjadi Donor Darah :</b>
+    <p>Donor darah adalah orang yang memberikan darah secara sukarela untuk maksud dan tujuan transfusi darah bagi orang
+        lain yang membutuhkan. Semua orang dapat menjadi donor darah jika memenuhi persyaratan yang berlaku.</p>
+    <b>APA SYARAT SYARAT UNTUK MENJADI DONOR DARAH ?</b>
+    <ol>
+        <li>Sehat jasmani dan rohani</li>
+        <li>Usia 17 sampai dengan 65 tahun.</li>
+        <li>Berat badan minimal 45 kg.</li>
+        <li>
+            Tekanan darah :
+            <ul>
+                <li>sistole 100 - 170</li>
+                <li>sistole 100 - 170</li>
+            </ul>
+        </li>
+        <li>Kadar haemoglobin 12,5g% s/d 17,0g%</li>
+        <li>Interval donor minimal 12 minggu atau 3 bulan sejak donor darah sebelumnya (maksimal 5 kali dalam 2 tahun)
+        </li>
+    </ol>
+    <b>JANGAN MENYUMBANGKAN DARAH JIKA :</b>
+    <ol>
+        <li>Mempunyai penyakit jantung dan paru paru</li>
+        <li>Menderita kanker</li>
+        <li>Menderita tekanan dara tinggi (hipertensi)</li>
+        <li>Menderita kencing manis (diabetes militus)</li>
+        <li>Memiliki kecenderungan perdarahan abnormal atau kelainan darah lainnya.</li>
+        <li>Menderita epilepsi dan sering kejang</li>
+        <li>Menderita atau pernah menderita Hepatitis B atau C.</li>
+        <li>Mengidap sifilis</li>
+        <li>Ketergantungan Narkoba.</li>
+        <li>Kecanduan Minuman Beralkohol</li>
+        <li>Mengidap atau beresiko tinggi terhadap HIV/AIDS</li>
+        <li>Dokter menyarankan untuk tidak menyumbangkan darah karena alasan kesehatan.</li>
+    </ol>
+    <b>PEMESANAN TANGGAL PELAKSANAAN MU</b>
+    <ol>
+        <li>Melalui himbauan yang dikirimkan oleh UTDD</li>
+        <li>
+            Penyesuaian tanggal atau permintaan :
+            <ul>
+                <li>Untuk hari kerja : minimum 2 - 3 bulan sebelum pelaksanaan.</li>
+                <li>Untuk hari minggu : disesuaikan dengan jadwal MU yang ada di MU</li>
+            </ul>
+        </li>
+        <li>Jika jumlah calon donor 500 - 1000 konfirmasi min 6 bulan - 1 tahun</li>
+        <li>Konfirmasi ulang perlu dilaksanakan 2 minggu sebelum pelaksanaan MU</li>
+        <li>Bila terjadi pembatalan, mohon memeberitahukan segera ke bagian penjadwalan donor PMI DKI Jakarta.</li>
+    </ol>
+    <b>PELAKSANAN DONOR</b>
+    <ol>
+        <li>Jumlah calon donor yang didaftarkan minimal 75 orang</li>
+        <li>Jumlah perolehan donor yang berhasil diambil minimal 80% dari calon pendonor yang terdaftar.</li>
+        <li>Apabila pendonor yang berhasil diambil kurang dari 80%, maka akan kami ingatkan kepada kordinator instansi
+            tersebut per telpon untuk meningkatkan perolehannya dalam pelaksanaan berikutnya dan apabila dalam
+            pelaksanaan berikutnya masih kurang dari 80%, maka akan kami ingatkan kembali yang ketiga kalinya dan
+            selanjutnya kami sarankan untuk rombongan ke PMI DKI Jakarta atau bergabung dengan instansi lain yang
+            terdekat.
+        </li>
+        <li>Pelaksanaan Donor Darah untuk Mobil Unit di instansi maksimal 3 jam untuk menjaga kualitas darah yang
+            baik.
+        </li>
+    </ol>
+    <b>BEBERAPA PANDUAN DONOR DARAH</b>
+    <ol>
+        <li>Tidur minimal 4 jam sebelum donor.</li>
+        <li>Makanlah 3 - 4 jam sebelum menyumbangkan darah. jangan menyumbangkan darah dengan perut kosong.</li>
+        <li>Minum lebih banyak dari biasanya pada hari mendonorkan darah (paling sedikit 3 gelas)</li>
+        <li>Setelah donor beristirahat paling sedikit 10 menit sambil menikmati makanan donor, sebelum kembali
+            beraktifitas.
+        </li>
+        <li>Kembali bekerja setelah donor darah tidak berbahaya untuk kesehatan.</li>
+        <li>Untuk menghindari bengkak di lokasi bekas jarum, hindari mengangkat benda berat selama 12 jam.</li>
+        <li>Banyak minum sampai 72 jam ke depan untuk mengembalikan stamina.</li>
+    </ol>
+    <b>APA YANG PERLU DISIAPKAN BILA AKAN MENYUMBANGKAN MELALUI MOBIL UNIT (MU)?</b>
+    <ol>
+        <li>Minimal jumlah calon donor 50 orang</li>
+        <li>Menyediakan ruangan yang luasnya sesuai dengan banyaknya donor, bersih, ventilasi cukup dan teduh (tidak di
+            bawah sinar matahari) dengan fasilitas air bersih dan tempat parkir untuk bus mobil unit.
+        </li>
+        <li>Menyediakan minimal 4 meja dan sejumlah kursi sesuai jumlah donor.</li>
+    </ol>
+    <b>Kriteria umum yang ditetapkan PMI adalah antara lain:</b>
+    <ul>
+        <li>calon donor harus berusia 17-60 tahun,</li>
+        <li>berat badan minimal 45 kg</li>
+        <li>tekanan darah 100-180 (sistole) dan 60-100 (diastole).</li>
+        <li>Jika berminat, calon donor dapat mengambil dan menandatangani formulir pendaftaran; lalu menjalani
+            pemeriksaan pendahuluan seperti kondisi berat badan, HB, golongan darah; serta dilanjutkan dengan
+            pemeriksaan dokter.
+        </li>
+        <li>Jika lulus, barulah darah dan contoh darah diambil.</li>
+        <li>Namun, harus diingat, demi menjaga kesehatan dan keamanan darah, individu yang antara lain memiliki kondisi
+            seperti alkoholik, penyakit hepatitis, diabetes militus, epilepsi, atau kelompok masyarakat risiko tinggi
+            mendapatkan AIDS serta mengalami sakit seperti demam atau influensa; baru saja dicabut giginya kurang dari
+            tiga hari; pernah menerima transfusi kurang dari setahun; begitu juga untuk yang belum setahun menato,
+            menindik, atau akupunktur; hamil; atau sedang menyusui untuk sementara waktu tidak dapat menjadi donor.
+        </li>
+    </ul>
+    <h3>Tunggu apa lagi, ayo ikut jadi donor darah!</h3>
+</div>
+@stop
+
+
