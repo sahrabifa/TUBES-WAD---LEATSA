7990e61ee11be87b9dc0153b1f5b6dbe05f60eba
PutriNelsa <@putrinelsa@student.telkomuniversity.ac.id>
2022-01-10 20:10:54 +0700

    main

--- TUBESWADFIX/routes/web.php
+++ TUBESWADFIX/routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('index');
 });
@@ -30,7 +31,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/me', [\App\Http\Controllers\AuthController::class, 'me'])->name('me');
     Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
     Route::post('/profile', [\App\Http\Controllers\AuthController::class, 'updateProfile'])->name('profile.update');
-
+ 
     Route::get('/donor', [\App\Http\Controllers\PMIController::class, 'donor'])->name('donor');
     Route::get('/donor/create', [\App\Http\Controllers\PMIController::class, 'donorCreate'])->name('donor.create');
     Route::post('/donor', [\App\Http\Controllers\PMIController::class, 'donorStore'])->name('donor.store');
@@ -38,6 +39,13 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/schedule/{schedule}/donor', [\App\Http\Controllers\PMIController::class, 'showScheduleDonor'])->name('schedule.show.donor');
 
+    Route::get('/user/{user}/edit', [\App\Http\Controllers\PMIController::class, 'editUser'])->name('user.edit');
+    Route::put('/user/{user}', [\App\Http\Controllers\PMIController::class, 'updateUser'])->name('user.update');
+    Route::delete('/user/{user}', [\App\Http\Controllers\PMIController::class, 'deleteUser'])->name('user.delete');
+});
+
 Route::get('/donor/about', [\App\Http\Controllers\PMIController::class, 'aboutDonor'])->name('donor.about');
 Route::get('/blood-stock', [\App\Http\Controllers\PMIController::class, 'bloodStock'])->name('blood-stock');
 Route::get('/contact', [\App\Http\Controllers\PMIController::class, 'contact'])->name('contact');
+Route::get('/schedule', [\App\Http\Controllers\PMIController::class, 'schedule'])->name('schedule');
+Route::get('/find-friend', [\App\Http\Controllers\PMIController::class, 'findFriend'])->name('find-friend');
