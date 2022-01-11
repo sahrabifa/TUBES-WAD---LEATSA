7fa19e51d4d737de2f85c46195c032cbab29098b
PutriNelsa <@putrinelsa@student.telkomuniversity.ac.id>
2022-01-12 00:54:44 +0700

    main

---
+++ TUBESWADFIX/resources/views/layout/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer-section devider-top">
+    <div class="container">
+        <div class="row float-right text-right">
+            <div class="col-12 py-5">
+                <p>© 2017 | ayodonor.pmi.or.id </p>
+
+                <p><b><a href="dashboard" class="text-dark">Palang Merah Indonesia</a></b> | Jl. Gatot Subroto Kav.96 Jakarta Selatan -
+                    Indonesia 12790
+                </p>
+
+                <p>Tel. +6221 7992325 Fax. +6221 7995188</p>
+            </div>
+        </div>
+    </div>
+</div>
